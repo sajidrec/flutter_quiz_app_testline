602c996317c8c3509357e44351cfd43c39b87a02
MD. Sajid Hossain <sajidrec@gmail.com>
2025-01-20 20:08:13 +0600

    - Fix high score not update on back press

--- lib/presentation/pages/result_page.dart
+++ lib/presentation/pages/result_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_quiz_app_testline/data/utils/constants.dart';
+import 'package:flutter_quiz_app_testline/presentation/pages/home_page.dart';
 import 'package:flutter_quiz_app_testline/presentation/utils/app_color.dart';
 import 'package:get/get.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -41,29 +42,42 @@ class _ResultPageState extends State<ResultPage> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text("Total Score"),
-        ),
-        body: SingleChildScrollView(
-          child: SizedBox(
-            width: Get.width,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const SizedBox(height: 10),
-                Icon(
-                  Icons.emoji_events,
-                  color: AppColor.primaryOrange,
-                  size: Get.height / 3,
-                ),
-                const SizedBox(height: 5),
-                Text(
-                  widget.score.toString(),
-                  style: TextStyle(fontSize: Get.height / 6),
-                ),
-              ],
+      child: PopScope(
+        canPop: false,
+        onPopInvokedWithResult: (didPop, result) {
+          Get.offAll(() => HomePage());
+        },
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text("Result"),
+            centerTitle: true,
+            backgroundColor: AppColor.primaryBlue,
+            foregroundColor: AppColor.white,
+          ),
+          body: SingleChildScrollView(
+            child: SizedBox(
+              width: Get.width,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const SizedBox(height: 10),
+                  Icon(
+                    Icons.emoji_events_outlined,
+                    color: AppColor.primaryOrange,
+                    size: Get.height / 3,
+                  ),
+                  const SizedBox(height: 5),
+                  Text(
+                    widget.score.toString(),
+                    style: TextStyle(fontSize: Get.width / 4),
+                  ),
+                  Text(
+                    "Your score",
+                    style: TextStyle(fontSize: Get.width / 6),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
